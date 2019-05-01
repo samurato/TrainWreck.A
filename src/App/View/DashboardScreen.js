16fabde5d35bdd4d8f42de8efc0b43de688dd453
Samrat Pant <samrat.sammie@gmail.com>
2019-05-01 21:38:01 +1000

    initialization of Semantic UI  with Menubar and Footer

---
+++ src/App/View/DashboardScreen.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {
+    Container,
+
+    Header,
+
+}
+
+from 'semantic-ui-react'
+
+const DashBoardScreen = () => (
+    <div>
+        <Container text style={{ marginTop: '7em' }}>
+            <Header as='h1'>Welcome to Train Wreck.A Dashboard</Header>
+            <p>Development in Progress</p>
+            <p> ATO Automatic Train Operations.</p>
+
+        </Container>
+    </div>
+);
+
+export default DashBoardScreen
